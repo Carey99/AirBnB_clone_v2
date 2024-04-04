e07f060cd213d7af8f43960aa1d0e1c83b92dcc2
Carey99 <edwinakidah1@gmail.com>
2024-04-04 16:42:23 -0700

    Compress before send

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """Generating a .tgz file"""
-from fabric.context_managers import lcd
 from datetime import datetime
-from fabric.operations import local
+from fabric.api import local
+import os
+import subprocess
 
 
 def do_pack():
-    local("mkdir -p versions")
-
+    os.makedirs("versions", exist_ok=True)
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    filename = f"web_static_{timestamp}.tgz"
-
-    with lcd("web_static"):
-        res = local(f"tar -czf ../versions/{filename} *", capture=True)
-
-    if res.succeeded:
-        return f"versions/{filename}"
-    else:
-        return None
+    output = f"versions/web_static_{timestamp}.tgz"
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except subprocess.CalledProcessError as e:
+        print(f"Error packing web_static: {e}")
+        output = None
+    except OSError as e:
+        print(f"Error accessing file: {e}")
+        output = None
+    return output
 if __name__ == "__main__":
     do_pack()
