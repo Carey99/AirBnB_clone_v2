b51a362b502e4dec20e67353b5fb62ebf1e776da
Carey99 <edwinakidah1@gmail.com>
2024-04-04 16:01:33 -0700

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Generating a .tgz file"""
+from fabric.context_managers import lcd
+from datetime import datetime
+from fabric.operations import local
+
+
+def do_pack():
+    local("mkdir -p versions")
+
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    filename = f"web_static_{timestamp}.tgz"
+
+    with lcd("web_static"):
+        res = local(f"tar -czf ../versions/{filename} *", capture=True)
+
+    if res.succeeded:
+        return f"versions/{filename}"
+    else:
+        return None
