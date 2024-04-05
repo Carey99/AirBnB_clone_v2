00e20b6db07304117cfa22a33ca428a5e0a54c11
Carey99 <edwinakidah1@gmail.com>
2024-04-04 17:02:18 -0700

    Newer

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,11 +10,14 @@ def do_pack():
     os.makedirs("versions", exist_ok=True)
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     output = f"versions/web_static_{timestamp}.tgz"
+    web_static_path = "/AirBnB_clone_v2/web_static/"
     try:
         print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
+        local("tar -cvzf {} {}".format(output, web_static_path))
         size = os.stat(output).st_size
         print("web_static packed: {} -> {} Bytes".format(output, size))
+
+        os.chmod(output, 0o664)
     except subprocess.CalledProcessError as e:
         print(f"Error packing web_static: {e}")
         output = None
