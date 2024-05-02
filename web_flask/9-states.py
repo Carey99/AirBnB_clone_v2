b51b11dba45223bd6adc2884e46fb4786a9217cc
Carey99 <edwinakidah1@gmail.com>
2024-05-02 09:29:09 -0700

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(self):
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
